a8b7ba19291fb89dbd7201ec0c18d998fcc19a81
AnasBenba <anasbenba123@gmail.com>
2023-05-27 13:34:13 +0200

    Day 2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 100_Days_of_code
-day 1: Band name generater project 
+day 1: Band name generater project
+day 2: Tip Calculator project
---
+++ Tip_Calcilator.py
@@ -0,0 +1,17 @@
+print("Welcome to the tip calculator!")
+
+# get the user input and convert it to a float
+
+total_bill = float(input("What was the total bill? $"))
+percentage_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+number_of_people = int(input("How many people to split the bill? "))
+
+# calculate how much each person should pay
+
+tip = total_bill * (percentage_tip / 100)
+bill_per_person = (total_bill + tip) / number_of_people
+result = round(bill_per_person, 2)
+
+# print the result
+
+print(f"Each person should pay: ${result}")
