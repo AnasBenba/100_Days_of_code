c636ded97d3f36487c061b697f9fc737e78e53cd
AnasBenba <anasbenba123@gmail.com>
2023-05-28 14:21:38 +0200

    add comments

--- Treasure_Island.py
+++ Treasure_Island.py
@@ -18,16 +18,24 @@ ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
 ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/______/
 *******************************************************************************''')
+
 print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
+
+# Get the user's choice at the first crossroad
 choice = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"\n')
+
 if choice == "right":
   print("You fell into a hole. Game Over.")
 elif choice == "left":
+  # Get the user's choice at the lake
   choice = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
+
   if choice == "swim":
     print("You get attacked by an angry trout. Game Over.")
   elif choice == "wait":
+    # Get the user's choice at the island
     choice = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n')
+    
     if choice == "red":
       print("It's a room full of fire. Game Over.")
     elif choice == "blue":
